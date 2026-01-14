d0569c84f8744595cb1d67ad6bdf8cf22806dcee
prajapatigagan <gaganprajapati121@gmail.com>
2026-01-14 23:09:09 +0530

    learing_java

---
+++ src/src/string_method.java
@@ -0,0 +1,57 @@
+public class string_method {
+    public static void main(String[] args) {
+        //normal string...
+        String name = "Gagan";
+        System.out.println(name);
+
+        //strings lenght...
+//        int value=name.length();
+//        System.out.println(value);
+
+        //lower case...
+//        String lstring=name.toLowerCase();
+//        System.out.println(lstring);
+
+        //upper case...
+//        String Alstring=name.toUpperCase();
+//        System.out.println(Alstring);
+
+        //trim method...
+//        String nontrimString="    Gagan    ";
+//        System.out.println(nontrimString);
+//        System.out.println(nontrimString.trim());
+
+        //subtract method...
+//        System.out.println(name.substring(3));
+//        System.out.println(name.substring(1,4));
+
+        //replace words...
+//        System.out.println(name.replace('G','M'));
+//        System.out.println(name.replace("gan","ush"));
+
+        //satrt with true/false...
+//        System.out.println(name.startsWith("Gag"));
+//        System.out.println(name.startsWith("mag"));
+//        System.out.println(name.endsWith("gan"));
+//        System.out.println(name.endsWith("pan"));
+
+        //character index...
+        //System.out.println(name.charAt(2));//start with 0..
+
+        //indexof use...
+//        String modifiedName="Gaggaann";
+//        System.out.println("gaggaann");
+//        System.out.println(modifiedName.indexOf("n"));
+//        System.out.println(modifiedName.indexOf("aa",2));
+
+//        System.out.println(modifiedName.lastIndexOf("a"));
+//        System.out.println(modifiedName.lastIndexOf("a",2));
+
+        //equal and equalignor case..
+//        System.out.println(name.equals("Gagan"));
+//        System.out.println(name.equalsIgnoreCase("gagan"));
+
+        System.out.println("I am escape sequence\"double quote");
+        //(\n,\t,\\,etc...)
+    }
+}

19da582dd978fbb881382b56825e080c1b254d6f
prajapatigagan <gaganprajapati121@gmail.com>
2026-01-27 22:20:03 +0530

    first commit

--- src/src/Example_recursion02.java
+++ src/src/Example_recursion02.java
@@ -29,7 +29,9 @@ public class Example_recursion02 {
 //        if(n%2==0){
 //            return gagan(x,n/2)*gagan(x,n/2);
 //        }
-////        n iss odd..
+/// /        n iss odd..
+///
+/// @return
 //        else{
 //            return gagan(x,n/2)*gagan(x,n/2)*x;
 //        }
@@ -71,16 +73,49 @@ public class Example_recursion02 {
 //    }
 
 //find the 1st and last occurance of an element in string
-public static void revstring(String str,int idx){
-    if(idx==0){
-        System.out.println(str.charAt(idx));
-        return;
+//public static int first=-1;
+//public static int last=-1;
+//
+//public static void find0ccurance(String str,int idx,char element){
+//    if(idx==str.length()){
+//        System.out.println(first);
+//        System.out.println(last);
+//        return;
+//    }
+//
+//    char current_char=str.charAt(idx);
+//    if(current_char==element){
+//     if(first==-1){
+//         first=idx;
+//     }
+//     else{
+//         last=idx;
+//     }
+//    }
+//
+//    find0ccurance(str,idx+1,element);
+//}
+//
+//    public static void main(String[] args) {
+//    String str="ahaarchaaah";
+//        find0ccurance(str,0,'a');
+//    }
+
+    //check sorted array....
+    public static boolean issorted(int arr[], int idx){
+    if(arr[idx]==arr.length){
+        return true;
     }
-    System.out.print(str.charAt(idx));
-    revstring(str, idx-1);
-}
+        if(arr[idx]<arr[idx+1]){
+           return issorted(arr,idx+1);
+        }
+       else{
+           return false;
+        }
+    }
+
     public static void main(String[] args) {
-        String str="gagan";
-        revstring(str,str.length()-1);
+        int arr[]={1,2,3,4,5,6,7};
+        System.out.println(issorted(arr,0));
     }
 }
